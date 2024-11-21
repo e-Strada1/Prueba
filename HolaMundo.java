2a7f16ba457dafacd4058c3a21224f82188b8223
e-Strada1 <estradariquelme2@gmail.com>
2024-11-21 19:16:50 +0100

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
 public class HolaMundo{
 
     public static void main(String args[]){
 
         System.out.println("Hola Mundo");
+        Scanner in = new Scanner(System.in);
+        String nombre;
+
+        System.out.println("Como te llamas?");
+        nombre = in.nextLine();
+
+        System.out.println("Hola "+nombre);
 
 
+        in.close();
     }
-
+    
 
 }
